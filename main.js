ab8045fd15e883c17ed955af5bc111b76fa52180
Othneil I Drew <codeguydrew@gmail.com>
2018-09-28 21:38:04 -0500

    Fixed syntax error

--- main.js
+++ main.js
@@ -3,8 +3,7 @@ $(function() {
   // Add background color to nav when user scrolls more than 50px down
   $(window).scroll(function() {
     if ($(this).scrollTop() > 50) {
-      $('#main-navbar
-      ').addClass('shadow-sm');
+      $('#main-navbar').addClass('shadow-sm');
     }
     if ($(this).scrollTop() < 50) {
       $('#main-navbar').removeClass('shadow-sm');
