5f2b8d67a5ea52d1f1a5018ce359820fba538191
Othneil I Drew <codeguydrew@gmail.com>
2018-10-01 13:12:15 -0500

    Update images file extensions to .jpg from .png

--- main.js
+++ main.js
@@ -18,22 +18,22 @@ $(function() {
 
     // Fetch pizza
     $.each(data.pizza, function(key, value) {
-      $('.pizza-grid').append('<div class="col-12 col-md-6 col-lg-4 col-xl-3 px-2"><div class="card menu-card my-2 shadow-none"><img class="card-img-top" src="https://www.othneildrew.com/johnspizza/images/menu-items/pizza/'+ value.photo +'" alt="'+ value.name +' Pizza"><div class="card-body"><h5 class="card-title text-left font-weight-bold">'+ value.name +' Pizza</h5><p class="card-text text-left">'+ value.description +'</p><p class="text-success text-left"><em>'+ value.calorie_per_serving +' calories</em></p><p class="grid-price">$'+ value.price +'</p></div><!--<div class="card-footer"><a class="btn btn-block shadow-sm btn-lg btn-CTA" href="order.html?item=garden" role="button">Start With This!</a></div>--></div></div>');
+      $('.pizza-grid').append('<div class="col-12 col-md-6 col-lg-4 col-xl-3 px-2"><div class="card menu-card my-2 shadow-none"><img class="card-img-top" src="images/menu/pizza/'+ value.photo +'" alt="'+ value.name +' Pizza"><div class="card-body"><h5 class="card-title text-left font-weight-bold">'+ value.name +' Pizza</h5><p class="card-text text-left">'+ value.description +'</p><p class="text-success text-left"><em>'+ value.calorie_per_serving +' calories</em></p><p class="grid-price">$'+ value.price +'</p></div><!--<div class="card-footer"><a class="btn btn-block shadow-sm btn-lg btn-CTA" href="order.html?item=garden" role="button">Start With This!</a></div>--></div></div>');
     });
 
     // Fetch toppings
     $.each(data.toppings, function(key, value) {
-      $('.toppings-grid').append('<div class="col-12 col-md-6 col-lg-4 col-xl-3 px-2"><div class="card menu-card my-2 shadow-none"><img class="card-img-top" src="https://www.othneildrew.com/johnspizza/images/menu-items/toppings/'+ value.photo +'" alt="'+ value.name +'"><div class="card-body"><h5 class="card-title text-left font-weight-bold">'+ value.name +' Pizza</h5><p class="text-success text-left"><em>'+ value.additional_calories +' calories</em></p><p class="grid-price">$'+ value.price +'</p></div></div></div>');
+      $('.toppings-grid').append('<div class="col-12 col-md-6 col-lg-4 col-xl-3 px-2"><div class="card menu-card my-2 shadow-none"><img class="card-img-top" src="images/menu/toppings/'+ value.photo +'" alt="'+ value.name +'"><div class="card-body"><h5 class="card-title text-left font-weight-bold">'+ value.name +' Pizza</h5><p class="text-success text-left"><em>'+ value.additional_calories +' calories</em></p><p class="grid-price">$'+ value.price +'</p></div></div></div>');
     });
 
     // Fetch drinks
     $.each(data.drinks, function(key, value) {
-      $('.drinks-grid').append('<div class="col-12 col-md-6 col-lg-4 col-xl-3 px-2"><div class="card menu-card my-2 shadow-none"><img class="card-img-top" src="https://www.othneildrew.com/johnspizza/images/menu-items/drinks/'+ value.photo +'" alt="'+ value.name +' Pizza"><div class="card-body"><h5 class="card-title text-left font-weight-bold">'+ value.name +' Pizza</h5></p><p class="text-success text-left"><em>'+ value.servings.twenty_ounce.calorie +' calories</em></p><p class="grid-price">$'+ value.servings.twenty_ounce.price +'</p></div></div></div>');
+      $('.drinks-grid').append('<div class="col-12 col-md-6 col-lg-4 col-xl-3 px-2"><div class="card menu-card my-2 shadow-none"><img class="card-img-top" src="images/menu/drinks/'+ value.photo +'" alt="'+ value.name +' Pizza"><div class="card-body"><h5 class="card-title text-left font-weight-bold">'+ value.name +' Pizza</h5></p><p class="text-success text-left"><em>'+ value.servings.twenty_ounce.calorie +' calories</em></p><p class="grid-price">$'+ value.servings.twenty_ounce.price +'</p></div></div></div>');
     });
 
     // Fetch desserts
     $.each(data.desserts, function(key, value) {
-      $('.dessert-grid').append('<div class="col-12 col-md-6 col-lg-4 col-xl-3 px-2"><div class="card menu-card my-2 shadow-none"><img class="card-img-top" src="https://www.othneildrew.com/johnspizza/images/menu-items/desserts/'+ value.photo +'" alt="'+ value.name +' Pizza"><div class="card-body"><h5 class="card-title text-left font-weight-bold">'+ value.name +' Pizza</h5><p class="card-text text-left">'+ value.description +'</p><p class="text-success text-left"><em>'+ value.calorie_per_serving +' calories</em></p><p class="grid-price">$'+ value.price +'</p></div></div></div>');
+      $('.dessert-grid').append('<div class="col-12 col-md-6 col-lg-4 col-xl-3 px-2"><div class="card menu-card my-2 shadow-none"><img class="card-img-top" src="images/menu/desserts/'+ value.photo +'" alt="'+ value.name +' Pizza"><div class="card-body"><h5 class="card-title text-left font-weight-bold">'+ value.name +' Pizza</h5><p class="card-text text-left">'+ value.description +'</p><p class="text-success text-left"><em>'+ value.calorie_per_serving +' calories</em></p><p class="grid-price">$'+ value.price +'</p></div></div></div>');
     });
 
 
